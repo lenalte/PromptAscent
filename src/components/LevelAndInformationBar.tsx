615e9b1fa22cfd20260b47714c1338beaca7fa8b
Lena Ertl <lertl@hm.edu>
2025-05-15 15:33:17 +0200

    icons added

--- src/components/LevelAndInformationBar.tsx
+++ src/components/LevelAndInformationBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from "next/image";
 
 
 const LevelAndInformationBar: React.FC<{ sidebarWidth: number; points: number }> = ({ sidebarWidth, points }) => {
@@ -15,18 +16,61 @@ const LevelAndInformationBar: React.FC<{ sidebarWidth: number; points: number }>
                 </div>
 
                 {/* Icons Container */}
-                <div className="flex pr-4 space-x-4">
-                    {/* Points Icon */}
-                    <div className="flex items-center">
-                        <span>{points} km</span> {/* Replace with your points icon or dynamic data */}
+                <div className="flex pr-4 space-x-8">
+
+                    <div className="flex items-center gap-2">
+                        <div className="flex items-center">
+                            <span>{points} km</span> {/* Replace with your points icon or dynamic data */}
+                        </div>
+                        <svg version="1.0" xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-foreground" viewBox="0 0 790.000000 790.000000"
+                            preserveAspectRatio="xMidYMid meet">
+
+                            <g transform="translate(0.000000,790.000000) scale(0.100000,-0.100000)"
+                                fill="currentColor" stroke="none">
+                                <path d="M4540 7180 l0 -390 -440 0 -440 0 0 -340 0 -340 -230 0 -230 0 0
+-370 0 -370 -235 0 -235 0 0 -580 0 -580 -230 0 -230 0 0 -415 0 -415 700 0
+700 0 0 -365 0 -365 -230 0 -230 0 0 -370 0 -370 -235 0 -235 0 0 -785 0 -785
+315 0 315 0 0 390 0 390 230 0 230 0 0 370 0 370 220 0 220 0 0 380 0 380 230
+0 230 0 0 380 0 380 230 0 230 0 0 385 0 385 225 0 225 0 0 220 0 220 -685 0
+-685 0 0 365 0 365 225 0 225 0 0 375 0 375 225 0 225 0 0 750 0 750 -315 0
+-315 0 0 -390z"/>
+                            </g>
+                        </svg>
                     </div>
 
-                    {/* Settings Icon */}
+                    {/* Backpack Icon */}
                     <div className="flex items-center">
-                        <span>bla</span> {/* Replace with settings icon */}
+                        <span className="text-yellow-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-foreground" viewBox="0 0 790.000000 790.000000"
+                                preserveAspectRatio="xMidYMid meet">
+
+                                <g transform="translate(0.000000,790.000000) scale(0.100000,-0.100000)"
+                                    fill="currentColor" stroke="none">
+                                    <path d="M3180 7375 l0 -195 -170 0 -170 0 0 -185 0 -185 -665 0 -665 0 0
+-180 0 -180 -155 0 -155 0 0 -200 0 -200 -195 0 -195 0 0 -965 0 -965 -195 0
+-195 0 0 -1130 0 -1130 195 0 195 0 0 -210 0 -210 350 0 350 0 0 -355 0 -355
+170 0 170 0 0 -195 0 -195 2140 0 2140 0 0 195 0 195 155 0 155 0 0 355 0 355
+350 0 350 0 0 210 0 210 165 0 165 0 0 1130 0 1130 -170 0 -170 0 -2 963 -3
+962 -177 3 -178 2 0 200 0 200 -165 0 -165 0 0 180 0 180 -680 0 -680 0 0 195
+0 195 -170 0 -170 0 0 185 0 185 -780 0 -780 0 0 -195z m1150 -380 l0 -185
+-370 0 -370 0 0 185 0 185 370 0 370 0 0 -185z m1720 -1555 l0 -980 -170 0
+-170 0 0 -170 0 -170 -485 0 -485 0 0 335 0 335 -725 0 -725 0 0 -335 0 -335
+-525 0 -525 0 0 170 0 170 -170 0 -170 0 0 980 0 980 2075 0 2075 0 0 -980z
+m-4540 -555 l0 -1135 -155 0 -155 0 0 1135 0 1135 155 0 155 0 0 -1135z m5228
+-2 l2 -1133 -150 0 -150 0 0 1135 0 1136 148 -3 147 -3 3 -1132z m-2388 -808
+l0 -325 -335 0 -335 0 0 325 0 325 335 0 335 0 0 -325z m-2150 -175 l0 -170
+545 0 545 0 0 -185 0 -184 148 -3 147 -3 3 -192 2 -193 395 0 395 0 0 195 0
+195 180 0 180 0 0 185 0 185 545 0 545 0 0 170 0 170 110 0 110 0 0 -1475 0
+-1475 -155 0 -155 0 0 -195 0 -195 -1750 0 -1750 0 0 195 0 195 -170 0 -170 0
+0 1475 0 1475 150 0 150 0 0 -170z m-1002 -907 l2 -753 -195 0 -195 0 0 755 0
+755 193 -2 192 -3 3 -752z m5882 2 l0 -755 -155 0 -155 0 0 755 0 755 155 0
+155 0 0 -755z"/>
+                                </g>
+                            </svg>
+                        </span>
                     </div>
                 </div>
-            </div>
+            </div >
         </>
     );
 };
--- src/components/ui/sidebarnew.tsx
+++ src/components/ui/sidebarnew.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Image from "next/image";
 
 
 interface SidebarProps {
@@ -21,7 +20,7 @@ const Sidebar: React.FC<SidebarProps> = ({ onToggle }) => {
             /* style={{ backgroundColor: "red" }} */
             aria-label="Sidebar"
         >
-            <div className={`h-full px-3 py-4 overflow-y-auto dark:bg-gray-800 sidebar-background`}>
+            <div className={`h-full px-1 py-4 overflow-y-auto dark:bg-gray-800 sidebar-background`}>
                 <ul className="space-y-2 font-medium">
                     {/*Profil Option*/}
                     <li>
@@ -30,16 +29,30 @@ const Sidebar: React.FC<SidebarProps> = ({ onToggle }) => {
                             className={`flex items-center p-4 rounded-lg dark:text-white hover:bg-[var(--sidebar-accent)] dark:hover:bg-[var(--sidebar-accent)] group sidebar-foreground`}
                             onClick={toggleSidebar}
                         >
-                            <Image
-                                className="full"
-                                src="./nobody_avatar.svg"
-                                alt="Avatar"
-                                width={30}
-                                height={30}
-                                style={{ width: "30px", height: "30px", marginRight: "10px", fill: "var(--sidebar-primary-foreground)" }}
-                            />
+                            <svg version="1.0" xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-foreground" viewBox="0 0 790.000000 790.000000"
+                                preserveAspectRatio="xMidYMid meet">
 
-                            <span className={`ms-3 ${collapsed ? 'hidden' : ''}`}>Profil</span>
+                                <g transform="translate(0.000000,790.000000) scale(0.100000,-0.100000)"
+                                    fill="currentColor" stroke="none">
+                                    <path d="M2900 7630 l0 -270 -265 0 -265 0 0 -270 0 -270 -270 0 -270 0 0
+-1040 0 -1040 270 0 270 0 0 -270 0 -270 -265 0 -265 0 0 -270 0 -270 -270 0
+-270 0 0 -785 0 -785 265 0 265 0 0 -270 0 -270 265 0 265 0 0 -775 0 -775
+1580 0 1580 0 0 775 0 775 265 0 265 0 0 270 0 270 265 0 265 0 0 785 0 785
+-270 0 -270 0 0 270 0 270 -265 0 -265 0 0 270 0 270 270 0 270 0 0 1040 0
+1040 -270 0 -270 0 0 270 0 270 -265 0 -265 0 0 270 0 270 -1040 0 -1040 0 0
+-270z m2080 -540 l0 -270 265 0 265 0 0 -1040 0 -1040 -265 0 -265 0 0 -270 0
+-270 265 0 265 0 0 -270 0 -270 265 0 265 0 0 -785 0 -785 -260 0 -260 0 0
+265 0 265 -270 0 -270 0 0 -1040 0 -1040 -260 0 -260 0 0 525 0 525 -525 0
+-525 0 0 -525 0 -525 -255 0 -255 0 0 1040 0 1040 -270 0 -270 0 0 -265 0
+-265 -260 0 -260 0 0 785 0 785 265 0 265 0 0 270 0 270 265 0 265 0 0 270 0
+270 -265 0 -265 0 0 1040 0 1040 265 0 265 0 0 270 0 270 1040 0 1040 0 0
+-270z"/>
+                                    <path d="M2900 5510 l0 -270 265 0 265 0 0 270 0 270 -265 0 -265 0 0 -270z" />
+                                    <path d="M4450 5510 l0 -270 265 0 265 0 0 270 0 270 -265 0 -265 0 0 -270z" />
+                                </g>
+                            </svg>
+
+                            <span className={`ms-3 ${collapsed ? 'hidden' : ''} text-foreground`}>Dein Name</span>
                         </a>
                     </li>
 
@@ -50,15 +63,29 @@ const Sidebar: React.FC<SidebarProps> = ({ onToggle }) => {
                             className={`flex items-center p-4 rounded-lg dark:text-white hover:bg-[var(--sidebar-accent)] dark:hover:bg-[--sidebar-accent)] group sidebar-foreground`}
                             onClick={toggleSidebar}
                         >
-                            <Image
-                                className="full"
-                                src="./leaderboard.svg"
-                                alt="Leaderboard"
-                                width={25}
-                                height={25}
-                                style={{ width: "25px", height: "25px", marginRight: "10px", fill: "var(--sidebar-primary-foreground)" }}
-                            />
-                            <span className={`flex-1 ms-3 whitespace-nowrap ${collapsed ? 'hidden' : ''}`}>Leaderboard</span>
+                            <svg version="1.0" xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-foreground" viewBox="0 0 790.000000 790.000000"
+                                preserveAspectRatio="xMidYMid meet">
+
+                                <g transform="translate(0.000000,790.000000) scale(0.100000,-0.100000)"
+                                    fill="currentColor" stroke="none">
+                                    <path d="M1450 7240 l0 -310 -495 0 -495 0 0 -1045 0 -1045 230 0 230 0 0
+-225 0 -225 265 0 265 0 0 -230 0 -230 223 -2 222 -3 3 -222 2 -223 240 0 240
+0 0 -200 0 -200 255 0 255 0 0 -225 0 -225 215 0 215 0 0 -470 0 -470 -375 0
+-375 0 0 -225 0 -225 -185 0 -185 0 0 -445 0 -445 1765 0 1765 0 0 445 0 445
+-182 2 -183 3 -3 223 -2 222 -390 0 -390 0 0 470 0 470 215 0 215 0 0 215 0
+215 255 0 255 0 0 210 0 210 260 0 260 0 0 225 0 225 205 0 205 0 0 230 0 230
+265 0 265 0 0 225 0 225 230 0 230 0 0 1045 0 1045 -495 0 -495 0 0 310 0 310
+-2500 0 -2500 0 0 -310z m4538 -1502 l-3 -1363 -102 -3 -103 -3 0 -219 0 -220
+-167 -2 -168 -3 -3 -207 -2 -208 -215 0 -215 0 0 -215 0 -215 -1060 0 -1060 0
+0 225 0 225 -200 0 -200 0 0 200 0 200 -185 0 -185 0 0 220 0 219 -102 3 -103
+3 -3 1363 -2 1362 2040 0 2040 0 -2 -1362z m-4538 -78 l0 -820 -160 0 -160 0
+0 220 0 219 -102 3 -103 3 -3 598 -2 597 265 0 265 0 0 -820z m5528 223 l-3
+-598 -102 -3 -103 -3 0 -219 0 -220 -160 0 -160 0 0 820 0 820 265 0 265 0 -2
+-597z m-2858 -3723 l0 -470 -170 0 -170 0 0 470 0 470 170 0 170 0 0 -470z
+m920 -1140 l0 -220 -1075 0 -1075 0 0 220 0 220 1075 0 1075 0 0 -220z"/>
+                                </g>
+                            </svg>
+                            <span className={`flex-1 ms-3 whitespace-nowrap ${collapsed ? 'hidden' : ''} text-foreground`}>Leaderboard</span>
                         </a>
                     </li>
                 </ul>
