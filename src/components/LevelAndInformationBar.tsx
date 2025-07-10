a3ede7a8db401bbaf5b2a03cda702cb9414253e0
Lena Ertl <lena4ltre@gmail.com>
2025-07-10 10:41:24 +0000

    nutze für die icons vor der punkteanzahl im leaderboard, das icon das au

--- src/components/LevelAndInformationBar.tsx
+++ src/components/LevelAndInformationBar.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import type { Level } from '@/data/level-structure';
 import { useUserProgress } from '@/context/UserProgressContext';
 import { BossIcon } from '@/components/icons/BossIcon';
+import { PointsIcon } from '@/components/icons/PointsIcon';
 
 interface LevelAndInformationBarProps extends React.HTMLAttributes<HTMLDivElement> {
     currentLevel: Level | null;
@@ -57,19 +58,7 @@ const LevelAndInformationBar: React.FC<LevelAndInformationBarProps> = ({ current
                         </div>
                     )}
                     <div className="flex items-center gap-2">
-                        <svg version="1.0" xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-primary-foreground" viewBox="0 0 790.000000 790.000000"
-                            preserveAspectRatio="xMidYMid meet">
-                            <g transform="translate(0.000000,790.000000) scale(0.100000,-0.100000)"
-                                fill="currentColor" stroke="none">
-                                <path d="M4540 7180 l0 -390 -440 0 -440 0 0 -340 0 -340 -230 0 -230 0 0
--370 0 -370 -235 0 -235 0 0 -580 0 -580 -230 0 -230 0 0 -415 0 -415 700 0
-700 0 0 -365 0 -365 -230 0 -230 0 0 -370 0 -370 -235 0 -235 0 0 -785 0 -785
-315 0 315 0 0 390 0 390 230 0 230 0 0 370 0 370 220 0 220 0 0 380 0 380 230
-0 230 0 0 380 0 380 230 0 230 0 0 385 0 385 225 0 225 0 0 220 0 220 -685 0
--685 0 0 365 0 365 225 0 225 0 0 375 0 375 225 0 225 0 0 750 0 750 -315 0
--315 0 0 -390z"/>
-                            </g>
-                        </svg>
+                        <PointsIcon className="h-8 w-8 text-primary-foreground" />
                         <span className="text-primary-foreground">{totalPoints} Punkte</span>
                     </div>
                     <div className="hidden sm:flex items-center">
