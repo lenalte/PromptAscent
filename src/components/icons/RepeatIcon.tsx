b9fc6a825e53278ec86075e2d27d4e8de0b2a745
Lena Ertl <lena4ltre@gmail.com>
2025-07-14 12:33:31 +0000

    wechsele das repeatIcon mit dem folgenden aus: <?xml version="1.0" stand

--- src/components/icons/RepeatIcon.tsx
+++ src/components/icons/RepeatIcon.tsx
@@ -6,20 +6,21 @@ interface RepeatIconProps extends React.SVGProps<SVGSVGElement> {}
 
 export const RepeatIcon: React.FC<RepeatIconProps> = ({ className, ...props }) => (
     <svg
-        width="690"
-        height="690"
-        viewBox="0 0 690 690"
-        fill="currentColor"
+        version="1.0"
         xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 790.000000 790.000000"
+        preserveAspectRatio="xMidYMid meet"
         className={cn("h-4 w-4", className)}
         {...props}
     >
-        <path d="M536 154H326V84H536V154Z" />
-        <path d="M536 154H606V434H536V154Z" />
-        <path d="M154 434H606V504H154V434Z" />
-        <path d="M154 224H84V504H154V224Z" />
-        <path d="M84 224H294V154H84V224Z" />
-        <path d="M294 154L364 224L294 294V154Z" />
+        <g
+            transform="translate(0.000000,790.000000) scale(0.100000,-0.100000)"
+            fill="currentColor"
+            stroke="none"
+        >
+            <path d="M5680 7370 l0 -530 -2260 0 -2260 0 0 -525 0 -525 -530 0 -530 0 0 -1060 0 -1060 530 0 530 0 0 1055 0 1055 2260 0 2260 0 0 -525 0 -525 530 0 530 0 2 263 3 262 263 3 262 2 0 260 0 260 265 0 265 0 0 530 0 530 -265 0 -265 0 0 265 0 265 -265 0 -265 0 0 265 0 265 -530 0 -530 0 0 -530z"/>
+            <path d="M6740 3160 l0 -1040 -2260 0 -2260 0 0 525 0 525 -530 0 -530 0 -2 -262 -3 -263 -262 -3 -263 -2 0 -260 0 -260 -265 0 -265 0 0 -530 0 -530 265 0 265 0 0 -265 0 -265 265 0 265 0 0 -265 0 -265 530 0 530 0 0 530 0 530 2260 0 2260 0 0 510 0 510 530 0 530 0 0 1060 0 1060 -530 0 -530 0 0 -1040z"/>
+        </g>
     </svg>
 );
 
