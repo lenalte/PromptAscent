2f66d8d7376f9cf6a77e665f0b2280ba9a7c2e4c
Lena Ertl <lena4ltre@gmail.com>
2025-07-15 15:41:34 +0000

    ist es möglich den hintergrund der progressbar nur im inventory zu --bac

--- src/components/ui/progressbar.tsx
+++ src/components/ui/progressbar.tsx
@@ -5,13 +5,15 @@ import { cn } from '@/lib/utils';
 interface ProgressBarProps {
     progress: number;
     progressText?: string; // Optional text to display on the progress bar
+    backgroundClassName?: string; // Optional class for the background
 }
 
-const ProgressBar: React.FC<ProgressBarProps> = ({ progress, progressText }) => {
+const ProgressBar: React.FC<ProgressBarProps> = ({ progress, progressText, backgroundClassName }) => {
     return (
         <div
             className={cn(
-                "w-full h-[15px] flex items-center sidebar-background relative overflow-hidden" // Removed py-[1px]
+                "w-full h-[15px] flex items-center relative overflow-hidden",
+                backgroundClassName || "sidebar-background" 
             )}
         >
             <div
