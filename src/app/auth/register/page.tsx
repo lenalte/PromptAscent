b1bd4319727c334a06020eec2f40f2d59f3e7e07
Lena Ertl <lertl@hm.edu>
2025-07-19 18:05:25 +0200

    back button deleted from register + icon updated

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,20 +1,9 @@
 
 import RegistrationForm from '@/components/auth/RegistrationForm';
-import Link from 'next/link';
-import { ChevronLeft } from 'lucide-react';
-import { EightbitButton } from '@/components/ui/eightbit-button';
 
 export default function RegisterPage() {
   return (
     <>
-       <div className="absolute top-4 left-4 z-10">
-        <Link href="/" passHref legacyBehavior>
-          <EightbitButton as="a">
-            <ChevronLeft className="mr-2 h-4 w-4" />
-            Zurück zur Startseite
-          </EightbitButton>
-        </Link>
-      </div>
       <RegistrationForm />
     </>
   );
